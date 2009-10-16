89fa3e44f5c95bd461836b79b1f8eea56a985eec
Sebastien Mondet <seb@mondet.org>
2009-10-16 14:34:38 +0200

    Code cleaning.

--- src/lib/dibrawi_std.ml
+++ src/lib/dibrawi_std.ml
@@ -15,7 +15,7 @@ module Str = struct
         try Some (rindex s c) with Not_found -> None
 
 
-    let nsplit str sep =
+    let nsplit str sep = (
         if str = "" then []
         else 
             (* str is non empty *)
@@ -23,25 +23,27 @@ module Str = struct
             let rec aux acc ofs =
                 if ofs >= 0 then (
                     match
-                    try Some (rfind_from str ofs sep)
-                    with Invalid_string -> None
+                        try Some (rfind_from str ofs sep)
+                        with Invalid_string -> None
                     with
                     | Some idx -> (* sep found *)
-                        let end_of_sep = idx + seplen - 1 in
-                        if end_of_sep = ofs (* sep at end of str *)
-                        then aux (""::acc) (idx - 1)
-                        else
-                            let token = sub str (end_of_sep + 1) (ofs - end_of_sep) in
-                            aux (token::acc) (idx - 1)
+                          let end_of_sep = idx + seplen - 1 in
+                          if end_of_sep = ofs (* sep at end of str *)
+                          then aux (""::acc) (idx - 1)
+                          else
+                              let token =
+                                  sub str (end_of_sep + 1) (ofs - end_of_sep) in
+                              aux (token::acc) (idx - 1)
                     | None     -> (* sep NOT found *)
-                        (sub str 0 (ofs + 1))::acc
-                )
-                else
-                    (* Negative ofs: the last sep started at the beginning of str *)
-                    ""::acc
+                          (sub str 0 (ofs + 1))::acc
+                ) else
+                    (* Negative ofs: the last sep started at the beginning of
+                     * str *)
+                    ("" :: acc)
             in
-            aux [] (length str - 1 )
-        
+            (aux [] (length str - 1 ))
+    )
+
 
 
 end
