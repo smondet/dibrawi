6c4315bf76e3d2ffbcf73efc90f8f2c184624092
Sebastien Mondet <seb@mondet.org>
2010-06-08 12:23:07 +0200

    Dibrawi.File_tree: Code cleaning.

--- src/lib/dibrawi.ml
+++ src/lib/dibrawi.ml
@@ -5,100 +5,96 @@ module Templating = Dibrawi_templating
 module Make = Dibrawi_make
 
 module Info = struct
-    let version = 0
-    let version_string = sprintf "The Dibrawi library, v %d" version
+  let version = 0
+  let version_string = sprintf "The Dibrawi library, v %d" version
 end
 
 module File_tree = struct
 
-    type file_tree_item = 
-        | Dir of string * string * file_tree
-        | File of string * string
-    and
+  type file_tree_item = 
+    | Dir of string * string * file_tree
+    | File of string * string
+  and
     file_tree = file_tree_item list
 
-    
-    let fold_tree
-    ?(dir=fun path name a -> a) ?(file=fun path name a -> ())
-    init tree = (
-        let rec parse current = function
-            | Dir (path, name, l) ->
-                let next = dir path name current in
-                Ls.iter (parse next) l
-            | File (path, name) ->
-                file path name current
-        in
-        Ls.iter (parse init) tree
-    )
+      
+  let fold_tree
+      ?(dir=fun path name a -> a) ?(file=fun path name a -> ())
+      init tree =
+    let rec parse current = function
+      | Dir (path, name, l) ->
+        let next = dir path name current in
+        Ls.iter (parse next) l
+      | File (path, name) ->
+        file path name current
+    in
+    Ls.iter (parse init) tree
+      
 
-    let print_tree =
-        fold_tree
-            ~dir:(fun path name indent -> 
-                printf "%s%s\n" (Str.make indent ' ') name;
-                indent + 2)
-            ~file:(fun path name indent ->
-                printf "%s%s\n" (Str.make indent ' ') name;)
-            0
+  let print_tree =
+    fold_tree
+      ~dir:(fun path name indent -> 
+        printf "%s%s\n" (Str.make indent ' ') name;
+        indent + 2)
+      ~file:(fun path name indent ->
+        printf "%s%s\n" (Str.make indent ' ') name;)
+      0
 
 
-    let string_path_list
-    ?(filter="\\.brtx$") ?(exclude=".*\\.svn.*") ?(url_prefix="")
-    tree = (
-        let filt = Pcre.regexp filter in
-        let excl = Pcre.regexp exclude in
-        let paths = ref [] in
-        fold_tree
-            ~file:(fun p n () ->
-                let full = p ^ "/" ^ n in
-                if (pcre_matches filt full) && not (pcre_matches excl full)
-                then (paths := (url_prefix ^ full) :: !paths);) () tree;
-        Ls.rev !paths
-    )
+  let string_path_list
+      ?(filter="\\.brtx$") ?(exclude=".*\\.svn.*") ?(url_prefix="") tree =
+    let filt = Pcre.regexp filter in
+    let excl = Pcre.regexp exclude in
+    let paths = ref [] in
+    fold_tree
+      ~file:(fun p n () ->
+        let full = p ^ "/" ^ n in
+        if (pcre_matches filt full) && not (pcre_matches excl full)
+        then (paths := (url_prefix ^ full) :: !paths);) () tree;
+    (Ls.rev !paths)
+  
 
-    (* Render a path compatible with Future.Path in batteries *)
-    let path_list 
-    ?(filter="\\.brtx$") ?(exclude=".*\\.svn.*") ?(prefix=[])
-    tree = (
-        let filt = Pcre.regexp filter in
-        let excl = Pcre.regexp exclude in
-        let paths = ref [] in
-        (* let current = ref prefix in *)
-        fold_tree
-            ~dir:(fun p n c ->
-                Opt.bind (fun cc -> 
-                    if not (pcre_matches excl n) then Some (n :: cc) else None) c) 
-            ~file:(fun p n c ->
-                Opt.may (fun c ->
-                    if (pcre_matches filt n)
-                    then (paths := (n :: c) :: !paths);) c
-            )
-            (Some prefix) tree;
-        Ls.rev !paths
-    )
+  (* Render a path compatible with Future.Path in batteries *)
+  let path_list 
+      ?(filter="\\.brtx$") ?(exclude=".*\\.svn.*") ?(prefix=[]) tree =
+    let filt = Pcre.regexp filter in
+    let excl = Pcre.regexp exclude in
+    let paths = ref [] in
+    (* let current = ref prefix in *)
+    fold_tree
+      ~dir:(fun p n c ->
+        Opt.bind (fun cc -> 
+          if not (pcre_matches excl n) then Some (n :: cc) else None) c) 
+      ~file:(fun p n c ->
+        Opt.may (fun c ->
+          if (pcre_matches filt n)
+          then (paths := (n :: c) :: !paths);) c
+      )
+      (Some prefix) tree;
+    (Ls.rev !paths)
+  
 
-    let str_and_path_list 
-    ?(filter="\\.brtx$") ?(exclude=".*\\.svn.*") ?(prefix=("", []))
-    tree = (
-        let filt = Pcre.regexp filter in
-        let excl = Pcre.regexp exclude in
-        let paths = ref [] in
-        (* let current = ref prefix in *)
-        fold_tree
-            ~dir:(fun p n c ->
-                Opt.bind (fun cc -> 
-                    if not (pcre_matches excl n)
-                    then Some (n :: cc) else None) c) 
-            ~file:(fun p n c ->
-                Opt.may (fun c ->
-                    if (pcre_matches filt n)
-                    then (
-                        let to_add = 
-                            ((fst prefix) ^ p ^ "/" ^ n, n :: c) in
-                        paths := to_add :: !paths;
-                    );) c)
-            (Some (snd prefix)) tree;
-        Ls.rev !paths
-    )
+  let str_and_path_list 
+      ?(filter="\\.brtx$") ?(exclude=".*\\.svn.*") ?(prefix=("", [])) tree =
+    let filt = Pcre.regexp filter in
+    let excl = Pcre.regexp exclude in
+    let paths = ref [] in
+    (* let current = ref prefix in *)
+    fold_tree
+      ~dir:(fun p n c ->
+        Opt.bind (fun cc -> 
+          if not (pcre_matches excl n)
+          then Some (n :: cc) else None) c) 
+      ~file:(fun p n c ->
+        Opt.may (fun c ->
+          if (pcre_matches filt n)
+          then (
+            let to_add = 
+              ((fst prefix) ^ p ^ "/" ^ n, n :: c) in
+            paths := to_add :: !paths;
+          );) c)
+      (Some (snd prefix)) tree;
+    (Ls.rev !paths)
 end
 
 module Data_source = struct
