702c79bc9d2e0efb308d9ad189f0183fd9e923fa
Sebastien Mondet <seb@mondet.org>
2010-09-22 18:45:57 +0200

    Continue implementation of Dibrawi.HTML.Template

--- src/lib/dibrawi_html_template.ml
+++ src/lib/dibrawi_html_template.ml
@@ -2,14 +2,55 @@ open Dibrawi_std
 open String_tree
 let ($) f x = f x
 
-type global_parameters = {
+type color = string
+type color_theme = [ `background | `main_font ] -> color option
+
+let no_theme = function
+  | `background 
+  | `main_font -> None
+
+type css_global_parameters = {
+  color_theme: color_theme;
+}
+
+let opt_fill_css param opt =
+  match opt with
+  | None -> empty
+  | Some value ->
+    str $ sprintf "   %s: %s;" param value
+
+let body params =
+  cat ~sep:(str "\n") [
+    str "body {";
+    opt_fill_css "background-color" (params.color_theme `background);
+    opt_fill_css "color" (params.color_theme `main_font);
+    str "}";
+  ]
+
+let css ?(color_theme=no_theme) l =
+  let params = {
+    color_theme = color_theme;
+  } in
+  let tree =
+    cat ~sep:(str "\n") [
+      str "<style type=\"text/css\">";
+      cat (Ls.map ~f:(fun f -> f params) l);
+      str "</style>";
+    ] in
+  `inline tree
+
+
+type html_template =
+    ?menu:string -> ?toc:string -> ?title:string -> ?footer:string ->
+    ?from:(string list) ->
+    string -> String_tree.t
+
+type html_global_parameters = {
   path_to_root: string list;
 }
 
-
-
-
 let make ?(css=`none) () =
+
   let insert_css css params =
     match css with
     | `none -> str ""
@@ -18,31 +59,46 @@ let make ?(css=`none) () =
         "<link rel=\"stylesheet\"  type=\"text/css\" href=\"%s\"/>"
         target
     | `link_from_root target ->
+      let depth = Ls.length params.path_to_root - 1 in
       str $ sprintf
         "<link rel=\"stylesheet\"  type=\"text/css\" href=\"%s/%s\"/>"
-        (Str.concat "/" params.path_to_root) target
+        ("./" ^ (Str.concat "/" (Ls.init depth (fun _ -> ".."))))
+        target
+    | `inline string_tree -> string_tree
   in
   let template_function  ?menu ?toc ?title ?footer ?(from=[]) content =
     let params = { path_to_root = from } in
     let opt_str_map f o = Opt.map_default (fun s -> str (f s)) empty o in
-    cat [
+    cat ~sep:(str "\n") [
       str "<!DOCTYPE html\n\
         PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n\
         \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n\
         <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n\
         <!-- Generated with Dibrawi, BraceTax, Sebib, and some more hacks -->\n\
         <head>\n\
-        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/>\n";
+        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/>";
       insert_css css params;
-      opt_str_map (sprintf "<title>%s</title>\n")  title;
-      str "</head>\n<body>\n";
-      opt_str_map (sprintf "<div class=\"dibrawititle\">%s</div>\n") title;
-      opt_str_map (sprintf "<div class=\"dibrawimenu\">%s</div>\n") menu;
-      opt_str_map (sprintf "<div class=\"dibrawitoc\">%s</div>\n") toc;
-      str $ sprintf "<div class=\"dibrawicontent\">%s</div>\n" content;
-      opt_str_map (sprintf "<div class=\"dibrawifooter\">%s</div>\n") footer;
+      opt_str_map (sprintf "<title>%s</title>")  title;
+      str "</head>\n<body>";
+      opt_str_map (sprintf "<div class=\"dibrawititle\">%s</div>") title;
+      opt_str_map (sprintf "<div class=\"dibrawimenu\">%s</div>") menu;
+      opt_str_map (sprintf "<div class=\"dibrawitoc\">%s</div>") toc;
+      str $ sprintf "<div class=\"dibrawicontent\">%s</div>" content;
+      opt_str_map (sprintf "<div class=\"dibrawifooter\">%s</div>") footer;
       str "</body></html>\n";
     ]
   in
-  template_function
+  (template_function:html_template)
 
+let _test () =
+  let doc (tmpl:html_template) = 
+    tmpl ~menu:"The Menu ..." ~from:[ "page.brtx"; "dir"; "dir"]
+      "The content..." in
+  let tmpla = make ~css:(`link_from_root "fromroot.css") () in
+  let tmplb = 
+    make ~css:(css [
+      body;
+    ]) () in
+  print $ doc tmpla;
+  print $ doc tmplb;
+  ()
